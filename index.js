1fb2d2cb6b6e374682f2fb1ce5c016e221979667
Devesh Maurya <deveshmaurya1996@gmail.com>
2022-09-17 05:59:56 +0000

    1 test case error

---
+++ index.js
@@ -0,0 +1,8 @@
+const name = require("../country/state/city/index");
+const fname = require("../utilities/utils/index");
+
+const getPeopleInCity = () => {
+  return fname(name);
+};
+module.exports = getPeopleInCity;
+console.log(getPeopleInCity);
